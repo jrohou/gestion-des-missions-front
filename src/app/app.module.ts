d318766f2341f8925f9f70167c4b003d6a1566cd
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-11-24 14:58:00 +0100

    Supression des components inutiles dans modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { TableauTemplateComponent } from './tableau-template/tableau-template.component';
-import { HeaderTableauMissionComponent } from './header-tableau-mission/header-tableau-mission.component';
-import { LigneTableauMissionComponent } from './ligne-tableau-mission/ligne-tableau-mission.component';
 import { TableauMissionComponent } from './tableau-mission/tableau-mission.component';
-import { DemandeMissionComponent } from './demande-mission/demande-mission.component';
 import { FormMissionComponent } from './form-mission/form-mission.component';
 import { PlanningComponent } from './planning/planning.component';
 import { PrimesComponent } from './primes/primes.component';
@@ -22,11 +18,7 @@ import { TableauNotesComponent } from './tableau-notes/tableau-notes.component';
     AppComponent,
     LoginComponent,
     NavBarComponent,
-    TableauTemplateComponent,
-    HeaderTableauMissionComponent,
-    LigneTableauMissionComponent,
     TableauMissionComponent,
-    DemandeMissionComponent,
     FormMissionComponent,
     PlanningComponent,
     PrimesComponent,
