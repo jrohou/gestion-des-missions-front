07f5d56ae1400da86ce16db52b5de06845358cf9
Gwenola LAINE <laineg.contact@gmail.com>
2017-11-24 15:44:33 +0100

    objets et ts form

--- src/app/shared/domain/mission.ts
+++ src/app/shared/domain/mission.ts
@@ -1,2 +1,9 @@
+import {Nature} from './nature'
+
 export class Mission {
+    id:number
+
+    constructor(dateDebut:Date, dateFin:Date, nature:Nature, villeDepart:string, villeArrivee:String, transport:string, montantPrime:number, statut:string){
+
+    }
 }
--- src/app/shared/domain/nature.ts
+++ src/app/shared/domain/nature.ts
@@ -1,2 +1,7 @@
 export class Nature {
+    id:number;
+
+    constructor(nom:string, dateDebutValidite:Date, dateFinValidite:Date, facturee:Boolean, versementPrime:Boolean, tauxJournalierMoyen:number){
+
+    }
 }
