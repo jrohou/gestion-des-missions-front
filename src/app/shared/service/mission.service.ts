12d6bccce3548b6a8b039da2c06e6329807ea87b
Gwenola LAINE <laineg.contact@gmail.com>
2017-11-24 15:20:32 +0100

    mission service post fait

--- src/app/shared/service/mission.service.ts
+++ src/app/shared/service/mission.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Mission } from '../domain/mission';
+import { environment } from '../../../environments/environment'
+import {Observable, BehaviorSubject} from "rxjs/Rx";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
 
 @Injectable()
 export class MissionService {
 
-  constructor() { }
+  subject:BehaviorSubject<Mission[]>  = new BehaviorSubject([])
 
+  constructor(private http: HttpClient) { }
+
+  sauvegarder(mission:Mission):void {
+    this.http.post<Mission>(environment.apiUrl + '/missions/', mission,
+       httpOptions).subscribe(col =>{
+         const collegues = this.subject.getValue()
+         collegues.push(col);
+         this.subject.next(collegues)
+       });
+      
+   }
 }
