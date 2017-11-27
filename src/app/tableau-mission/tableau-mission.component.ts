c7345d5c48cdb8297674fe1f1b0033d2c2929bc2
Gwenola LAINE <laineg.contact@gmail.com>
2017-11-27 17:21:14 +0100

    modale suppression

--- src/app/tableau-mission/tableau-mission.component.ts
+++ src/app/tableau-mission/tableau-mission.component.ts
@@ -8,18 +8,25 @@ import { Mission } from '../shared/domain/mission'
   styleUrls: ['./tableau-mission.component.css']
 })
 export class TableauMissionComponent implements OnInit {
-item:String="employe"
- public missions:Mission[]= [];
+  item:String="employe"
+  public missions:Mission[]= [];
+  public suppression:Boolean;
+  public missionASupprimer:Mission;
 
   constructor(private missionService:MissionService) { }
-
-  
 
   ngOnInit() {
     this.missionService.lister().subscribe(listeMissions => {this.missions = listeMissions;})
   }    
+
+  validerSuppression(mission:Mission){
+    this.suppression = true;
+    this.missionASupprimer = mission;
+  }
+
   supprimer(id:number){
     this.missionService.supprimerMission(id);
+    this.suppression=false;
   }
 
 }
