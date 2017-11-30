3e49855d035e2e2bf5e04f343675a5bc3ce12c99
kevin <kev.aubree@gmail.com>
2017-11-30 10:10:24 +0100

    blabla

--- src/app/tableau-note-mission-view/tableau-note-mission-view.component.ts
+++ src/app/tableau-note-mission-view/tableau-note-mission-view.component.ts
@@ -4,33 +4,32 @@ import { Mission } from '../shared/domain/mission';
 import { NotesService } from '../shared/service/notes.service';
 import { Note } from '../shared/domain/note';
 import { ActivatedRoute } from '@angular/router';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import * as moment from 'moment';
 
 
 @Component({
   selector: 'app-tableau-note-mission-view',
   templateUrl: './tableau-note-mission-view.component.html',
-  styleUrls: ['./tableau-note-mission-view.component.css','../../assets/font-awesome-4.7.0/css/font-awesome.min.css']
+  styleUrls: ['./tableau-note-mission-view.component.css', '../../assets/font-awesome-4.7.0/css/font-awesome.min.css']
 })
 export class TableauNoteMissionViewComponent implements OnInit {
 
-  idmission:number;
-  mission:Mission = null;
-  notes:Note[]= [];
+  idmission: number;
+  mission: Mission = null;
+  notes: Note[] = [];
   public noteASupprimer: Note;
   public suppression: Boolean;
   closeResult: string;
 
-  constructor(private missionService:MissionService, private noteService:NotesService, private route:ActivatedRoute,  private modalService: NgbModal) {
-    
-   
+  constructor(private missionService: MissionService, private noteService: NotesService, private route: ActivatedRoute, private modalService: NgbModal) {
+
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {this.idmission = params['idmission'];});
-    this.noteService.listerNoteMission(this.idmission).subscribe(listeNotes => {this.notes = [];listeNotes.forEach(note=>{this.notes.push(note)})});
-    this.missionService.trouverMission(this.idmission).subscribe(miss => {console.log(miss);this.mission = miss});
+    this.route.params.subscribe(params => { this.idmission = params['idmission']; });
+    this.noteService.listerNoteMission(this.idmission).subscribe(listeNotes => { this.notes = []; listeNotes.forEach(note => { this.notes.push(note) }) });
+    this.missionService.trouverMission(this.idmission).subscribe(miss => { console.log(miss); this.mission = miss });
   }
 
   openSupprimer(contentSup, note: Note) {
@@ -39,7 +38,7 @@ export class TableauNoteMissionViewComponent implements OnInit {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    }); 
+    });
   }
 
   private getDismissReason(reason: any): string {
@@ -48,10 +47,10 @@ export class TableauNoteMissionViewComponent implements OnInit {
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
-      return  `with: ${reason}`;
+      return `with: ${reason}`;
     }
   }
-  
+
   supprimer(id: number) {
     this.noteService.supprimerNote(id);
   }
